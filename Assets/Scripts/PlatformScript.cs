f522ce495ea862d0f746f22a4ec4a65da763b4ed
Omer Kubalas <omer.kubalas@bilgiedu.net>
2021-03-25 01:50:07 +0300

    Level Adjustment

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -14,6 +14,7 @@ public class PlatformScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 30;
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         OriginalPlatPos = this.gameObject.transform.position;
         shot = false;
