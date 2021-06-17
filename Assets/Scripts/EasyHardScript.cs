4a515f822d9c430d5be6dbb6da1e4423f0811a95
Ghina <ghina_7alabi@hotmail.com>
2021-06-17 10:02:19 +0300

    EasyHard

---
+++ Assets/Scripts/EasyHardScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class EasyHardScript : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public Color selectColor, deselectColor;
+
+    //void Update
+    //{
+
+    //}
+
+    void ISelectHandler.OnSelect(BaseEventData eventData)
+    {
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().color = selectColor;
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().fontSize = 45;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().color = deselectColor;
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().fontSize = 40;
+    }
+
+    //public void OnPointerEnter(PointerEventData pointerEventData)
+    //{
+    //    gameObject.GetComponentInChildren<TextMeshProUGUI>().color = selectColor;
+    //    gameObject.GetComponentInChildren<TextMeshProUGUI>().fontSize = 55;
+    //}
+
+    //public void OnPointerExit(PointerEventData pointerEventData)
+    //{
+    //    gameObject.GetComponentInChildren<TextMeshProUGUI>().color = deselectColor;
+    //    gameObject.GetComponentInChildren<TextMeshProUGUI>().fontSize = 40;
+    //}
+}
